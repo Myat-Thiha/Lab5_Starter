990b65531377c910a2a274dee566e97a9ca941fb
Myat-Thiha <mthiha@go.pasadena.edu>
2025-05-06 21:11:41 -0700

    finished unit test

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Lab 5 - Starter
 Myat Thiha
-Make sure you make a PR to your own repo's main and not the class' repo!! Otherwise you will lose points!!
+
+1. No, because the “message” feature involves multiple components working together that includes typing input, sending data over a network, and updating the UI or server. These are integration-level behaviors and are harder to isolate in a unit test.
+
+2. Yes, because checking the message length is a simple, self-contained logic that can be tested independently. A unit test can easily verify that messages longer than 80 characters are rejected and messages under that limit are allowed.
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,45 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const synth = window.speechSynthesis;
+  const voiceSelect = document.getElementById('voice-select');
+  const speakButton = document.querySelector('button');
+  const textInput = document.getElementById('text-to-speak');
+  const faceImage = document.querySelector('img');
+
+  function populateVoices() {
+    const voices = synth.getVoices();
+
+    voices.forEach((voice, index) => {
+      const option = document.createElement('option');
+      option.textContent = `${voice.name} (${voice.lang})`;
+      option.value = index;
+      voiceSelect.appendChild(option);
+    });
+  }
+
+  populateVoices();
+
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoices;
+  }
+
+  speakButton.addEventListener('click', function() {
+    const text = textInput.value.trim();
+    if (!text || voiceSelect.value === 'select') return;
+
+    const utterance = new SpeechSynthesisUtterance(text);
+    const voices = synth.getVoices();
+    utterance.voice = voices[voiceSelect.value];
+
+    utterance.onstart = () => {
+      faceImage.src = 'assets/images/smiling-open.png';
+    };
+
+    utterance.onend = () => {
+      faceImage.src = 'assets/images/smiling.png';
+    };
+
+    synth.speak(utterance);
+  });
 }
